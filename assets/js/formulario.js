74ed8b84b9a740421c96ddc6fcbf3814089156f2
vcamargo04 <viniciusac2005@hotmail.com>
2023-05-31 16:53:47 -0300

    Css e Js basico das páginas criado.

---
+++ assets/js/formulario.js
@@ -0,0 +1,42 @@
+document
+  .querySelector(".conteudo")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nameInput = document.getElementById("name");
+    const lastNameInput = document.getElementById("lastName");
+    const emailInput = document.getElementById("email");
+    const telInput = document.getElementById("tel");
+    const enderecoInput = document.getElementById("endereco");
+    const numInput = document.getElementById("num");
+    const cepInput = document.getElementById("cep");
+    const bairroInput = document.getElementById("bairro");
+    const cidadeInput = document.getElementById("cidade");
+    const idCustoSelect = document.getElementById("idCusto");
+
+    // Realizando a verificação do codigo
+    if (
+      nameInput.value.trim() === "" ||
+      lastNameInput.value.trim() === "" ||
+      emailInput.value.trim() === "" ||
+      telInput.value.trim() === "" ||
+      enderecoInput.value.trim() === "" ||
+      numInput.value.trim() === "" ||
+      cepInput.value.trim() === "" ||
+      bairroInput.value.trim() === "" ||
+      cidadeInput.value.trim() === "" ||
+      idCustoSelect.value === "0"
+    ) {
+      alert(
+        "Por favor, preencha todos os campos antes do envio do formulário."
+      );
+      return;
+    }
+
+    // Caso todos os campos sejam preenchidos o formulario será enviado
+    {
+      alert("O formulário foi validado com sucesso.");
+      return;
+    }
+    this.submit();
+  });
